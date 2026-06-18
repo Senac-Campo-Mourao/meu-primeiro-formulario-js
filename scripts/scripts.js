37d186344720d13cc2a5d0a7925a5004888a4d2e
Pedro Manoel Rosa Domingues Santos <pedromanoel0509@hotmail.com>
2026-06-18 17:31:39 -0300

    Aula 3 - Validação cpf

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,51 @@
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[.-]/g, "");
+
+    if (cpf.length !== 11 || /^(\\d)\\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    let soma = 0;
+    let resto;
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    const ultimoCaracter = parseInt(cpf.substring(9, 10));
+    if (resto !== ultimoCaracter) {
+        return false;
+    }
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+
+    const ultimaValidacao = parseInt(cpf.substring(10, 11));
+    if (resto !== ultimaValidacao) {
+        return false;
+    }
+
+    return true;
+}
+
+document.getElementById('clientCPF').addEventListener('blur', function () {
+    const cpf = this.value;
+    if (!validarCPF(cpf)) {
+        alert("CPF inválido. Por favor, insira um CPF válido");
+        this.value = "";
+    }
+});
+
 document.getElementById('formCadastro').addEventListener('submit', function (event) {
     event.preventDefault();
     console.log("Entrou no cadastro de cliente");
